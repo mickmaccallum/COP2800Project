b8c7f27ad2765ae6573d4ee5f1c5ae12d4e46b3b
Mick MacCallum <micksmaccallum@gmail.com>
2014-11-06 16:27:36 -0500

    Attempted.

--- src/pkgfinal/project/ExceptionHandling.java
+++ src/pkgfinal/project/ExceptionHandling.java
@@ -100,3 +100,38 @@ class SlopePoint {
     }
 }
 
+// Possibly incomplete:
+
+class Challenge{
+private static double height(int feet, int inches) {
+        return (feet * 30.48) + (inches * 2.54); // 1 foot = 30.48 cm // 1 inch = 2.54 cm
+    }
+    
+ public static void main(String args[]){
+ Scanner scanner=new Scanner(System.in);
+ boolean continueLoop = true;
+ 
+ do {
+     try {
+        System.out.println("Enter height in feet:");
+        int feet = scanner.nextInt();
+        
+        System.out.println("and in inches:");
+        int inches = scanner.nextInt();
+        
+        if (feet < 0 || inches < 0) {
+            throw new Exception();
+        }
+
+        double result = height(feet,inches);
+        System.out.println("Result:"+result+" cm");
+        
+        continueLoop = false;
+     } catch (InputMismatchException e) {
+         System.out.println();
+        continue;   
+     } catch (Exception e) {
+         System.out.println("Please enter positive values only.");
+     }
+} while (continueLoop);
+
