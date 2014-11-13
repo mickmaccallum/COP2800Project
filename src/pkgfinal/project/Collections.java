764f2186a03df34a6fb7b783e8b65afd600018d0
Mick MacCallum <micksmaccallum@gmail.com>
2014-11-13 12:40:31 -0500

    Finished collections.

--- src/pkgfinal/project/Collections.java
+++ src/pkgfinal/project/Collections.java
@@ -1,14 +1,14 @@
 
 package pkgfinal.project;
-import java.util.Scanner;
-import java.util.ArrayList;
+import java.util.*;
+
 
 public class Collections {
 
     public static void largestValueInArrayList() {
         
         Scanner scanner = new Scanner(System.in);
-        ArrayList<Integer> list = new ArrayList<Integer>();
+        ArrayList<Integer> list = new ArrayList<>();
 
 	System.out.println("Enter the number of integers that will be inserted: ");
 	int size = scanner.nextInt();
@@ -31,11 +31,10 @@ public class Collections {
         System.out.println("The largest value: " + max);
     }
     
-    
     public static void largestValueAndIndexInArrayList() {
         
         Scanner scanner = new Scanner(System.in);
-    	ArrayList<Integer> al = new ArrayList<Integer>();
+    	ArrayList<Integer> al = new ArrayList<>();
         
         System.out.println("Enter the number of integers that will be inserted: ");
 	int size = scanner.nextInt();
@@ -62,7 +61,7 @@ public class Collections {
     
     public static void lengthOfLongestStringInArrayList() {
         Scanner scanner = new Scanner(System.in);
-    	ArrayList<String> al = new ArrayList<String>();
+    	ArrayList<String> al = new ArrayList<>();
     	
         System.out.println("Enter 5 Strings to store in arraylist:");
         
@@ -82,5 +81,61 @@ public class Collections {
         }
     
         System.out.println("TheLength of longest string in arraylist:" + max);
+    }
+    
+    public static void stack() {
+        Scanner scanner=new Scanner(System.in);
+        int sum = 0;
+
+        System.out.println("Enter the 1st number:");
+        int num1 = scanner.nextInt();
+
+        System.out.println("Enter the 2nd number:");
+        int num2 = scanner.nextInt();
+
+        System.out.println("Enter the 3rd number:");
+        int num3 = scanner.nextInt();
+ 
+        Stack <Integer> st = new Stack<> (); 
+
+        System.out.println(st);
+        
+        st.add(num1);
+        System.out.println(st);
+
+        st.add(num2);
+        System.out.println(st);
+
+        st.add(num3);
+        System.out.println(st);
+        
+        st.pop();
+        System.out.println(st);
+    }
+    
+    public static void main(String args[]) {
+    	Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter ten elements: ");
+
+        ArrayList<Integer> a1 = new ArrayList<>();
+
+        for (int i = 0; i < 10; i++) {
+            a1.add(scanner.nextInt());
+        }
+
+        ArrayList<Integer> a2 = new ArrayList<>(a1);
+
+        a1.set(a1.size() - 1, -7);
+
+        System.out.println("ArrayList 1:" + print(a1));
+    	System.out.println("ArrayList 2:" + print(a2));
+    }
+    
+    private static String print(ArrayList<Integer> a) {
+    	String ret = "";
+		
+        ret = a.stream().map((a1) -> a1 + " ").reduce(ret, String::concat);
+		
+        return ret;
     }
 }
