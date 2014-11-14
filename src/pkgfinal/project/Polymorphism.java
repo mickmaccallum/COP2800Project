011d8f2980a589f11d1975e7a0bb1751f21049f2
Mick MacCallum <micksmaccallum@gmail.com>
2014-11-13 19:54:31 -0500

    Moar polymorphism

--- src/pkgfinal/project/Polymorphism.java
+++ src/pkgfinal/project/Polymorphism.java
@@ -1,5 +1,6 @@
-
 package pkgfinal.project;
+import java.text.DecimalFormat;
+import java.util.Scanner;
 
 public class Polymorphism {
     
@@ -78,5 +79,98 @@ public class Polymorphism {
             int result = overload.test('a');   
             System.out.println("Result " + result);   
 	}   
+    }
+    
+    private class Animal {
+
+	public void move() {
+            System.out.println("Animals can move");
+	}
+    }
+
+    private class Dog extends Animal {
+
+        @Override
+	public void move() {
+            System.out.println("Dogs can walk and run");
+	}
+    }
+
+    public class MethodOverridding {
+
+	public void main() {
+            Animal animal = new Animal();
+            animal.move();
+        
+            Dog dog = new Dog();
+            dog.move();
+        }
+    }
+    
+    class AreasOfShapes {
+        public void main() {
+            Scanner sc = new Scanner(System.in);
+            System.out.print("**Menu**\n1. Area of Rectangle\n2. Area of Square\n3. Area of Circle\n4. Area of Triangle\nEnter your choice :");
+
+            int ch = sc.nextInt();	
+            Area ar = new Area();
+	
+            switch(ch) {
+                case 1:		
+                    double a, b;
+                    System.out.print("Enter length :");
+                    a = sc.nextDouble();
+                    System.out.print("Enter breadth :");
+                    b = sc.nextDouble();
+                    a = ar.area(a, b);
+                    System.out.print("Area of rectangle is :" + a);	
+                    break;
+                case 2:
+                    double s;
+                    System.out.print("Enter edge :");
+                    s = sc.nextDouble();
+                    s = ar.area(s, s);
+                    System.out.print("Area of square is :" + s);
+                    break;
+                case 3:
+                    double r;
+                    System.out.print("Enter radius :");
+                    r = sc.nextDouble();
+                    r = ar.area(r);
+				
+                    DecimalFormat df = new DecimalFormat("#.0000");
+                    String formatted = df.format(r);
+
+                    System.out.print("Area of circle is :" + formatted);
+                    break;
+		case 4:
+                    double c;
+                    System.out.print("Enter side 1 :");
+                    a = sc.nextDouble();
+                    System.out.print("Enter side 2 :");
+                    b = sc.nextDouble();
+                    System.out.print("Enter side 3 :");
+                    c = sc.nextDouble();
+                    c = ar.area(a,b,c);
+                    System.out.print("Area of triangle is :" + c);
+                    break;
+		default:
+                    System.out.print("Wrong Option!!");
+            }
+	}
+    }
+
+    private class Area {
+        public double area(double a, double b) {
+            return a * b;
+        }
+
+        public double area(double r) {
+            return Math.PI  * (float)Math.pow(r, 2.0);
+        }
+
+        public double area(double a, double b, double c) {
+            return (a * b * c) / 2.0;
+        }
     }   
 }
